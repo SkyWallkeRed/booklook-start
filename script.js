9beac246d9f57279094dfdf254bcddd096e50a83
דוד אוחנה <davidmail000@gmail.com>
2018-04-28 22:20:28 +0200

    fixed bug ISBN find right book

--- script.js
+++ script.js
@@ -2,15 +2,15 @@ console.log('script-js  ON');
 var $book = $('.book');
 var bookResult = [];
 var bData;
-//handlebars2
-// var source2 = $('#display-template2').html();
-// var template2 = Handlebars.compile(source);
-// var newHTML2 = template(bookResult);
-//handlebars
 var source = $('#display-template').html();
 var template = Handlebars.compile(source);
 var newHTML = template(bookResult);
+var $headerLogo = $('.lookBookHeader');
 
+$headerLogo.on('click', function() {
+    $book.empty();
+    bookResult = [];
+});
 var fetch = function($serchData) {
     $.ajax({
         method: "GET",
@@ -18,10 +18,7 @@ var fetch = function($serchData) {
         url: 'https://www.googleapis.com/books/v1/volumes?q=' + $serchData + 'intitle:',
         url: 'https://www.googleapis.com/books/v1/volumes?q=' + $serchData + ' subject:',
         success: function(data) {
-
-
             findBook(data, $serchData);
-
         },
         error: function(jqXHR, textStatus, errorThrown) {
             console.log(textStatus);
@@ -30,27 +27,15 @@ var fetch = function($serchData) {
 };
 
 function findBook(data, $serchData) {
-    console.log(data);
-    console.log($serchData);
-
+    $book.empty();
+    bookResult = [];
     for (let i = 0; i < data.items.length; i++) {
-
-        if (data.items[i].volumeInfo.title) {
-            console.log('books colection found')
-            renderResultList(data, i);
-        } else if (data.items[i].volumeInfo.description) {
-            console.log('books colection found')
-            renderResultList(data, i);
-        } else if (data.items[i].volumeInfo.industryIdentifiers[1].identifier == $serchData) {
-            console.log('books colection found')
-            return renderResultList(data, i);
-        } else {
-            // newHTML = { title: 'Book Not Found' }
-            // $book.append(`<h1>` + newHTML.title + `</h1>`);
-            console.log('serch dont match ')
-        }
+        true ?
+            renderResultList(data, i) :
+            console.log('serch NOT found ')
     }
 }
+
 
 $('.search-book').on('click', function() {
     var $serchData = $('.form-control').val();
@@ -59,40 +44,111 @@ $('.search-book').on('click', function() {
 })
 
 $('.book').on('click', '.bookTitle', function() {
-
     var clickedBookisbn = $(this).attr("data-isbn");
     console.log(clickedBookisbn)
+    var clickedBook = $('[data-isbn =' + clickedBookisbn + ']');
     $(document).ready(function() {
-        $("p,h3,img ").toggle(this);
+        $("p,h3,img ").toggle(this); //the 'this' maks a strange transition in DOM ?? ask ..
     });
 })
 
-// render title only 
 function renderResultList(data, i) {
-
-    // creat an OBJ to append with handlebars
-    let title = data.items[i].volumeInfo.title;
-    let isbn = data.items[i].volumeInfo.industryIdentifiers[0].identifier;
-    let authors = data.items[i].volumeInfo.authors;
-    let description = data.items[i].volumeInfo.description;
-    let img = data.items[i].volumeInfo.imageLinks.smallThumbnail;
     let obj = {
         book: [{
-            title: title,
-            isbn: isbn,
-            authors: authors,
-            description: description,
-            img: img,
+            title: data.items[i].volumeInfo.title,
+            isbn: data.items[i].volumeInfo.industryIdentifiers[0].identifier,
+            authors: data.items[i].volumeInfo.authors,
+            description: data.items[i].volumeInfo.description,
+            img: data.items[i].volumeInfo.imageLinks.smallThumbnail,
         }, ]
     }
     bookResult.push(obj)
-
     if (jQuery.inArray(bookResult[i].book[i], bookResult) !== -1) {
         bookResult.push(obj)
     }
     for (let z = 0; z < bookResult[i].book.length; z++) {
         var newHTML = template(bookResult[i].book[z]);
-
         $book.append(newHTML);
     }
 }
+
+
+
+//###################################################################################
+
+// console.log('script-js  ON');
+// var $book = $('.book');
+// var bookResult = [];
+// var bData;
+// var source = $('#display-template').html();
+// var template = Handlebars.compile(source);
+// var newHTML = template(bookResult);
+
+// var fetch = function($serchData) {
+//     $.ajax({
+//         method: "GET",
+//         url: 'https://www.googleapis.com/books/v1/volumes?q=' + $serchData + 'isbn:',
+//         url: 'https://www.googleapis.com/books/v1/volumes?q=' + $serchData + 'intitle:',
+//         url: 'https://www.googleapis.com/books/v1/volumes?q=' + $serchData + ' subject:',
+//         success: function(data) {
+//             findBook(data, $serchData);
+//         },
+//         error: function(jqXHR, textStatus, errorThrown) {
+//             console.log(textStatus);
+//         }
+//     });
+// };
+
+// function findBook(data, $serchData) {
+//     $book.empty();
+//     console.log($serchData);
+//     for (let i = 0; i < data.items.length; i++) {
+//         (true) ?
+//         renderResultList(data, i)
+//             // book not found massege
+//             :
+//             console.log('serch dont match ') // serch for 'dasdasfsfgasdasfdAS' brings an eror
+//     }
+// }
+
+// $('.search-book').on('click', function() {
+//     var $serchData = $('.form-control').val();
+//     fetch($serchData);
+// })
+
+// $('.book').on('click', '.bookTitle', function() {
+
+//     var clickedBookisbn = $(this).attr("data-isbn");
+//     console.log('Clicked book ISBN: ' + clickedBookisbn)
+//     var clickedBook = $('[data-isbn =' + clickedBookisbn + ']');
+//     // NEED TO FIX TOGGLE TO A SACIFIC BOOK AND LOAD WITH 'TOGGLE - HIDE ' 
+//     $(document).ready(function() {
+//         $("p,h3,img ").toggle(this); //the 'this' maks a strange transition in DOM ?? ask ..
+
+//     });
+// })
+
+// function renderResultList(data, i) {
+//     let obj = {
+//         book: [{
+//             title: data.items[i].volumeInfo.title,
+//             isbn: data.items[i].volumeInfo.industryIdentifiers[0].identifier,
+//             authors: data.items[i].volumeInfo.authors,
+//             description: data.items[i].volumeInfo.description,
+//             img: data.items[i].volumeInfo.imageLinks.smallThumbnail,
+//         }, ]
+//     }
+//     bookResult.push(obj)
+//     if (jQuery.inArray(bookResult[i].book[i], bookResult) !== -1) {
+//         bookResult.push(obj)
+//     }
+//     for (let z = 0; z < bookResult[i].book.length; z++) {
+//         var newHTML = template(bookResult[i].book[z]);
+//         $book.append(newHTML);
+
+//     }
+
+// }
+
+
+//$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
